4043831962cb217cf55a6cf2c1ab366b8f39ea53
Joe Casabona <jcasabona@gmail.com>
2015-01-21 08:36:55 -0500

    Added some styles for content area

--- wp-content/themes/parsec/header.php
+++ wp-content/themes/parsec/header.php
@@ -22,7 +22,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'parsec' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<div class="site-branding contain">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
