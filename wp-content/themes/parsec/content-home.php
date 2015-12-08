34b528dea3db4b304e2ef824ab8be208a655a08f
Joe Casabona <jcasabona@gmail.com>
2015-12-07 22:25:49 -0500

    Added flexbox support for homepage

--- wp-content/themes/parsec/content-home.php
+++ wp-content/themes/parsec/content-home.php
@@ -6,24 +6,28 @@
  */
 ?>
 
-<div class="impact-area clear">
+<section class="impact-area clear">
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="featured-image">
 			<?php the_post_thumbnail( 'medium' ); ?>
 		</div>
 	<?php endif; ?>
-	
+
 	<div class="featured-content">
 		<?php the_content(); ?>
 	</div>
-</div>
+</section>
 
-<aside class="box-half">
-	<?php dynamic_sidebar( 'home-left' ); ?>
-</aside>
 
-<aside class="box-half">
-	<div class="widget">
-			<h2>I Write</h2>
-			<?php parsec_latest_post(); ?>
-</aside>
+<section class="home-columns">
+	<aside class="box-half">
+		<?php dynamic_sidebar( 'home-left' ); ?>
+	</aside>
+
+	<aside class="box-half">
+		<div class="widget">
+				<h2>I Write</h2>
+				<?php parsec_latest_post(); ?>
+		</div>
+	</aside>
+</section>
