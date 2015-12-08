af9163bb1898d694ffb3b33260d6eb759f89faf4
Joe Casabona <jcasabona@gmail.com>
2015-12-07 23:51:07 -0500

    Forms and RWD

--- wp-content/themes/parsec/content-home.php
+++ wp-content/themes/parsec/content-home.php
@@ -9,7 +9,7 @@
 <section class="impact-area clear">
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="featured-image">
-			<?php the_post_thumbnail( 'medium' ); ?>
+			<?php the_post_thumbnail( 'large' ); ?>
 		</div>
 	<?php endif; ?>
 
