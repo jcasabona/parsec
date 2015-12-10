0337837f626f8d302bbcf7a9ce299f0d13b60412
Joe Casabona <jcasabona@gmail.com>
2015-12-09 23:32:14 -0500

    Featured image class

--- wp-content/themes/parsec/content-page.php
+++ wp-content/themes/parsec/content-page.php
@@ -7,11 +7,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		if ( has_post_thumbnail() ) {
-			the_post_thumbnail();
-		}
-	?>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="featured-image">
+			<?php the_post_thumbnail(); ?>
+		</div>
+	<?php endif; ?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
