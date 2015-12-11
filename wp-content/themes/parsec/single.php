3d858ec0f66a8a2863515d779304e66bce0add90
Joe Casabona <jcasabona@gmail.com>
2015-12-10 23:25:12 -0500

    Minor style changes

--- wp-content/themes/parsec/single.php
+++ wp-content/themes/parsec/single.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
+				if ( ( comments_open() || get_comments_number() ) && is_single() ) :
 					comments_template();
 				endif;
 			?>
