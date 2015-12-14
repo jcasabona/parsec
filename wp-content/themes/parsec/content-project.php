d81d6f3986695599acc7ba4200c927edbb9fcdd7
Joe Casabona <jcasabona@gmail.com>
2015-12-14 09:18:18 -0500

    Simplify project page

--- wp-content/themes/parsec/content-project.php
+++ wp-content/themes/parsec/content-project.php
@@ -7,7 +7,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="featured-image">
-			<?php the_post_thumbnail(); ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
 		</div>
 	<?php endif; ?>
 	<header class="entry-header">
