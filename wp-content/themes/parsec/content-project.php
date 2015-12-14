75c7020e0ae48c2372b19b091b0a4092a175f1b0
Joe Casabona <jcasabona@gmail.com>
2015-12-14 00:00:15 -0500

    Adjust header

--- wp-content/themes/parsec/content-project.php
+++ wp-content/themes/parsec/content-project.php
@@ -13,24 +13,4 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'parsec' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'parsec' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
